8fe767c78cfe9b746130d14e0ce688e03572ab96
Teju Chinthoti <saiteja1699@gmail.com>
2023-05-12 10:55:16 +0530

    Adding default login capability

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,116 +1,65 @@
-import { setAuthedUser } from '../actions/authedUser';
+import React, { useState } from 'react';
 import { _getUser } from '../utils/api';
-import { useState } from 'react';
 import { connect } from 'react-redux';
+import { setAuthedUser } from '../actions/authedUser';
 import { useNavigate, useLocation } from 'react-router-dom';
-import {
-  Button,
-  FormControl,
-  Typography,
-  TextField,
-  Container,
-  Alert,
-  Box
-} from '@mui/material';
 
-const Login = (props) => {
-  const [error, setError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+const Login = ({ users, dispatch }) => {
+  const [selectedUser, setSelectedUser] = useState('');
 
   const navigate = useNavigate();
   const location = useLocation();
   const path = location.pathname;
-
-  const handleLogin = (e) => {
-    e.preventDefault(e);
-
-    if (!username || !password) {
-      setError(true);
-      setErrorMessage('Please enter username and password');
-    } else {
-      _getUser(username, password).then((user) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const getUser = async () => {
+      _getUser(selectedUser).then((user) => {
         if (user) {
-          setError(false);
-          setErrorMessage('');
-          props.dispatch(setAuthedUser(user));
-        } else {
-          setError(true);
-          setErrorMessage('Incorrect username or password');
+          dispatch(setAuthedUser(user));
         }
       });
-    }
+    };
+    getUser();
     if (path === '/') {
       navigate('/home');
     }
   };
 
   return (
-    <Container
-      variant='outlined'
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 2,
-        alignItems: 'center',
-        maxWidth: 400,
-        mx: 'auto',
-        my: 4,
-        py: 3,
-        px: 2
-      }}
-    >
-      <Typography level='h4' component='h1' sx={{ textAlign: 'center' }}>
-        <b>Employee Polls</b>
-      </Typography>
-      <Box className='box-center'>
-        {error && (
-          <Alert data-testid='login-error' severity='error'>
-            Incorrect username or password
-          </Alert>
-        )}
-      </Box>
-      <Typography level='body2' sx={{ textAlign: 'center' }}>
-        Sign in to continue
-      </Typography>
-      <FormControl>
-        <TextField
-          name='username'
-          value={username}
-          placeholder='Enter username'
-          type='text'
-          onChange={(e) => setUsername(e.target.value)}
-        ></TextField>
-        <TextField
-          sx={{
-            maxWidth: 400,
-            mt: 2
-          }}
-          name='password'
-          placeholder='Enter password'
-          type='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        ></TextField>
-        <Button
-          sx={{
-            minWidth: 300,
-            mt: 2
-          }}
-          variant='outlined'
-          fullWidth
-          type='submit'
-          data-testid='login-btn'
-          onClick={handleLogin}
-        >
-          Login
-        </Button>
-      </FormControl>
-    </Container>
+    <div className='form-container'>
+      <form>
+        <h1>Login</h1>
+        <label>Select a user:</label>
+        <div>
+          {users.map((user) => {
+            const { id, avatarURL, name } = user;
+
+            return (
+              <div key={id}>
+                <img src={avatarURL} alt={name} />
+                <label>{name}</label>
+                <input
+                  type='radio'
+                  name='user'
+                  value={user.id}
+                  onChange={(e) => setSelectedUser(e.target.value)}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <button className='user-button' type='submit' onClick={handleSubmit}>
+          Submit
+        </button>
+      </form>
+    </div>
   );
 };
 
-const mapStateToProps = (props) => props;
+const mapStateToProps = ({ users }) => {
+  return {
+    users: Object.keys(users).map((id) => users[id])
+  };
+};
 
 export default connect(mapStateToProps)(Login);
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,32 +1,31 @@
-import { _getUsers, _getQuestions, _saveQuestion, _saveQuestionAnswer, allUsers } from "./_DATA.js";
+import {
+  _getUsers,
+  _getQuestions,
+  _saveQuestion,
+  _saveQuestionAnswer,
+  allUsers
+} from './_DATA.js';
 
-export function getInitialData () {
-  return Promise.all([
-    _getUsers(),
-    _getQuestions(),
-  ]).then(([users, questions]) => ({
-    users,
-    questions,
-  }))
+export function getInitialData() {
+  return Promise.all([_getUsers(), _getQuestions()]).then(
+    ([users, questions]) => ({
+      users,
+      questions
+    })
+  );
 }
 
-export function _getUser(userId, password) {
-
+export function _getUser(userId) {
   return new Promise((resolve) => {
     setTimeout(
-      () =>
-        resolve(
-          allUsers
-            .filter((user) => user.id === userId && user.password === password)
-            .pop()
-        ),
+      () => resolve(allUsers.filter((user) => user.id === userId).pop()),
       1000
     );
   });
 }
 
-export function saveQuestion (question) {
-  return _saveQuestion(question)
+export function saveQuestion(question) {
+  return _saveQuestion(question);
 }
 
 export function saveQuestionAnswer(props) {
