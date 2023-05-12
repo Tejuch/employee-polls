c4cb1160c70bc76ed17d98c15695d925f928b63e
Teju Chinthoti <saiteja1699@gmail.com>
2023-05-12 15:27:31 +0530

    Adding tests for savequestion and answer

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,45 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import configureStore from 'redux-mock-store';
+import { store } from '../index';
 import Login from '../components/Login';
-import thunk from 'redux-thunk';
 
-const middlewares = [thunk];
-const mockStore = configureStore(middlewares);
-
-describe('NewQuestion', () => {
-  let mockedStore;
-
-  beforeEach(() => {
-    mockedStore = mockStore({
-      users: {
-        testuser: {
-          id: 'testuser',
-          password: 'abc345',
-          name: 'Test user',
-          avatarURL: 'https://i.pravatar.cc/150?img=3',
-          answers: {},
-          questions: []
-        }
-      },
-      authedUser: 'testuser'
-    });
-  });
-
-  it('should login with correct credentials', () => {
+describe('Login', () => {
+  it('will create a snapshot', () => {
     const component = render(
       <MemoryRouter>
-        <Provider store={mockedStore}>
+        <Provider store={store}>
           <Login />
         </Provider>
       </MemoryRouter>
     );
-    const loginButton = component.getByTestId('login-btn');
-    fireEvent.click(loginButton);
-    const loginError = component.getByTestId('login-error');
-    expect(loginError).toBeInTheDocument();
+    expect(component).toMatchSnapshot();
   });
 });
